71df96c4a26bcc5de60b04c6182e581bf3e364ef
franciuma <franciuma@uma.es>
2021-04-05 07:04:56 -0700

    Clase centro

--- src/main/java/es/uma/informatica/jpa/demo/Centro.java
+++ src/main/java/es/uma/informatica/jpa/demo/Centro.java
@@ -1,5 +1,6 @@
 package es.uma.informatica.jpa.demo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,50 +8,53 @@ import javax.persistence.Id;
 @Entity
 public class Centro {
 	
-	@Id @GeneratedValue
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
 	private Long id;
-	private String NombreCompleto;
-	private String EmailInstitucional;
-	private String EmailPersonal;
-	private String Telefono;
-	private String Movil;
+	@Column(name = "Nombre",nullable = false,unique = true)
+	private String nombre;
+	@Column(name = "Direccion",nullable = false)
+	private String direccion;
+	@Column(name = "Telefono de Conserjeria",length = 9)
+	private String TLF_Conserjeria;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNombreCompleto() {
-		return NombreCompleto;
-	}
-	public void setNombreCompleto(String nombreCompleto) {
-		NombreCompleto = nombreCompleto;
-	}
-	public String getEmailInstitucional() {
-		return EmailInstitucional;
-	}
-	public void setEmailInstitucional(String emailInstitucional) {
-		EmailInstitucional = emailInstitucional;
-	}
-	public String getEmailPersonal() {
-		return EmailPersonal;
-	}
-	public void setEmailPersonal(String emailPersonal) {
-		EmailPersonal = emailPersonal;
-	}
-	public String getTelefono() {
-		return Telefono;
-	}
-	public void setTelefono(String telefono) {
-		Telefono = telefono;
-	}
-	public String getMovil() {
-		return Movil;
-	}
-	public void setMovil(String movil) {
-		Movil = movil;
+	
+	public String getNombre() {
+		return nombre;
 	}
 	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public String getTLF_Conserjeria() {
+		return TLF_Conserjeria;
+	}
+	
+	public void setTLF_Conserjeria(String tLF_Conserjeria) {
+		TLF_Conserjeria = tLF_Conserjeria;
+	}
+
+	@Override
+	public String toString() {
+		return "Centro [id=" + id + ", nombre=" + nombre + ", direccion=" + direccion + ", TLF_Conserjeria="
+				+ TLF_Conserjeria + "]";
+	}
 
 }
